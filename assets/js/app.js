3a01e1909f54e09609416c999acd52237cdcc7b3
8GutiMu <you@example.com>
2017-06-26 17:09:36 -0500

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,7 @@
 // http://laboratoria.cuadra.co:9339/api/v1/students/
+var api = {
+    url: 'http://laboratoria.cuadra.co:9339/api/v1/students/'
+};
 
 var cargarPagina = function () {
     cargarAlumnas();
@@ -7,19 +10,28 @@ var cargarPagina = function () {
 
 var cargarAlumnas = function(){
     
-     $.ajax({
-        url: 'http://laboratoria.cuadra.co:9339/api/v1/students/',
-        method: 'GET',
-        dataType: 'jsonp',
-        success: function (response) {
-            console.log("respuesta ", response);
-        },
-        error: function (error) {
-            console.log("error ", error);
-        }
-    });  
+     
+ $.getJSON(api.url, function (alumnas) {
+        alumnas.forEach(function(alumna){
+            renderAlumnas(alumna)
+        });
+    });
+       
 }
 
+
+var renderAlumnas = function(alumna){
+    var $nombre = alumna.name;
+    var $lastName = alumna.pLastName;
+    var $last2Name = alumna.mLastName;
+    
+    var $containerAsist = $("#asist");
+    var $li = $("<li type='checkbox'> </li>");
+    $li.text($nombre);
+    $containerAsist.append($li)
+    
+    
+}
 $(document).ready(cargarPagina);
 
 
