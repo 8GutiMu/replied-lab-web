23248b43fb23f9a299900cf4f0ed07fb856d84c9
8GutiMu <you@example.com>
2017-06-26 16:14:41 -0500

    API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
 // http://laboratoria.cuadra.co:9339/api/v1/students/
+
+var cargarPagina = function () {
+    cargarAlumnas();
+   
+};
+
+var cargarAlumnas = function(){
+    
+     $.ajax({
+        url: 'http://laboratoria.cuadra.co:9339/api/v1/students/',
+        method: 'GET',
+        dataType: 'jsonp',
+        success: function (response) {
+            console.log("respuesta ", response);
+        },
+        error: function (error) {
+            console.log("error ", error);
+        }
+    });  
+}
+
+$(document).ready(cargarPagina);
+
+
